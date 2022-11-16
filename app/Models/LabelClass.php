5e2f02578853eee185d287f0f888933856c56a43
Martim <mitram790@gmail.com>
2022-11-16 17:49:05 +0000

    LabelClass ORM

--- app/Models/LabelClass.php
+++ app/Models/LabelClass.php
@@ -9,4 +9,9 @@ class LabelClass extends Model
 {
     use HasFactory;
     public $timestamps=false;
+    public $table= "label_class";
+
+    public function labels(){
+        return $this->belongsToMany(Label::class,"label_label_class","id_label","id_label_class");
+    }
 }
