ebffd63dc191116341a5f7040ae840666579e0fb
Martim <mitram790@gmail.com>
2022-11-16 17:50:01 +0000

    User ORM

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,4 +11,12 @@ class User extends Authenticatable
 
     // Don't add create and update timestamps in database.
     public $timestamps  = false;
+    
+    public function projects(){
+        return $this->belongsToMany(Project::class,"id_project");
+    }
+
+    public function tasks(){
+        return $this->belongsToMany(Task::class,"assignmnt","id_task","id_users");
+    }
 }
