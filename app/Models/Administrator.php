2f87234ebc978f64fcd88555398d271ab4d4557a
Martim <mitram790@gmail.com>
2022-11-16 12:28:33 +0000

    Administrator relations

--- app/Models/Administrator.php
+++ app/Models/Administrator.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Administrator extends Model
+use App\Models\User;
+
+class Administrator extends User
 {
     use HasFactory;
     public $timestamps=false;
     public $table="administrator";
+    
+    public function user(){
+        return $this->hasOne(User::class,'id','id_users');
+    }
 }
