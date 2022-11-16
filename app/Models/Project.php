39375a53b6cffcec3d89327fbc49c652ed871ce8
Martim <mitram790@gmail.com>
2022-11-16 17:49:45 +0000

    Project ORM

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,5 +10,15 @@ class Project extends Model
     use HasFactory;
     public $timestamps  = false;
     public $table = "project";
-        
+
+    public function coordinator(){
+        return $this->hasOne(User::class,"id","id_coordinator");
+    }
+    public function collaborators(){
+
+        return $this->belongsToMany(User::class,"collaborator","id_project");
+    }
+    public function boards(){
+        return $this->hasMany(Board::class,"id_project");
+    }
 }
