f471b3b240e12df7d9d461c1e715f651dd433879
Emanuel Gestosa <emanuel-367@hotmail.com>
2022-11-19 22:38:19 +0000

    add username parameter to register

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,18 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     *@var array
+     */
+    protected $fillable = [
+        'name',
+        'username',
+        'email', 
+        'password',
+    ];
+
     // Don't add create and update timestamps in database.
     public $timestamps  = false;
     
