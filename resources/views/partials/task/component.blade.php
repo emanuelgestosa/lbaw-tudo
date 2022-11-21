f39ed937ba31aa89387a3144d5dae70c6b6ffb12
Mariana Rocha <root@HejSokoly.localdomain>
2022-11-21 19:13:36 +0000

    design to board and vertical

--- resources/views/partials/task/component.blade.php
+++ resources/views/partials/task/component.blade.php
@@ -1,16 +1,25 @@
+@extends('layouts.app')
+<link href="{{ asset('/css/board.css') }}" rel="stylesheet">
+
+@yield('content')
+
+@section('content')
+
 <article class="task-component" data-id="{{ $task->id }}">
-<header>
-  <h2 class="task-name"> {{ $task->name}} </h2>
-  <h3 class="task-description"> {{$task->description}} </h3>
-</header>
+  <h1 class="page_name"> Task - {{ $task->name}} </h1>
+  <h3> {{$task->description}} </h3>
 <section class="label-container">
+        <b> Labels: </b>
         @foreach ($task->labels()->get() as $label)
-           @include('partials.label.tag',['label'=>$label])
-        @endforeach
+          @include('partials.label.tag',['label'=>$label]) <br>
+        @endforeach     
 </section>
 <section class="comment-container">
+        <b> Comments: </b>
         @foreach ($task->comments()->get() as $comment)
            @include('partials.comment.component',['comment'=>$comment])
         @endforeach
 </section>
 </article>
+
+@endsection
