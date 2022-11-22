fcb41d21d702cb0b7bf8f24099f429ad1565b108
Leandro <jose.leandro2002@gmail.com>
2022-11-22 23:43:19 +0000

    min due_date is now tomorrow

--- resources/views/pages/add_task.blade.php
+++ resources/views/pages/add_task.blade.php
@@ -15,7 +15,7 @@
   <label for="description">Description</label>
   <input id="description" type="text" name="description">
   <label for="due_date">Due_date</label>
-  <input id="due_date" type="date" name="due_date" value="" pattern="\d{4}-\d{2}-\d{2}" >
+  <input id="due_date" type="date" name="due_date" min="{{date('Y-m-d', strtotime('+1 days'))}}" value="" pattern="\d{4}-\d{2}-\d{2}" >
 
   <button type="submit">
     Save
