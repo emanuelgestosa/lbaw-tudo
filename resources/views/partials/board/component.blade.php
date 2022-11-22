357ee86f2f8d3c467a8430c1c5b6dbf05630b605
Mariana Rocha <root@HejSokoly.localdomain>
2022-11-22 17:18:53 +0000

    board page design edited

--- resources/views/partials/board/component.blade.php
+++ resources/views/partials/board/component.blade.php
@@ -5,10 +5,11 @@
 
 @section('content')
 
-
+<h1 class="page_name">{{$board->name}} </h1>
 <article class="board" id="{{$board->id}}">
+
     @foreach ($board->verticals()->get() as $vertical)
-        @include('partials.vertical.component',['vertical',$vertical])
+       @include('partials.vertical.component',['vertical',$vertical])
     @endforeach
 </article>
 
