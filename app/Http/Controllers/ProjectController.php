61d7f344f151f80f7e206ef84e7b31b503c5bb64
Emanuel Gestosa <emanuel-367@hotmail.com>
2022-12-07 11:09:09 +0000

    organizing project pages

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,18 +10,18 @@ class ProjectController extends Controller
 {
     public function show($id) {
       $project = Project::find($id);
-      return view('partials.project.component', ['project'=>$project]);
+      return view('pages.project.main', ['project'=>$project]);
     }
 
     public function invites($id){
       $project = Project::find($id);
-      return view('pages.projectInvites', ['project'=>$project]);
+      return view('pages.project.invites', ['project'=>$project]);
     }
     
     public function showCreate($user_id)
     {
       $user = Auth::user();
-      return view('pages.add_project', ['user' => $user]);
+      return view('pages.project.create', ['user' => $user]);
     }
 
     public function create(Request $request){
