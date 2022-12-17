c231c032520ed3f863530c903740eb364e28d849
Mariana Rocha <root@HejSokoly.localdomain>
2022-12-17 13:15:54 +0000

    dropdown working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,8 +47,16 @@
               <li>
                 <a class="nav-link" href="#">Oi </a>
               </li>
-              <li>
-                <a class="nav-link" href="#">Tudo? </a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
               </li>
           </div>
         </nav>
