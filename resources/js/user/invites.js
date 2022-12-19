62c6d88faf4bac8021a3596c95a53f781dc7bb99
Martim <mitram790@gmail.com>
2022-12-19 18:09:45 +0000

    user invites now use sendRequest

--- resources/js/user/invites.js
+++ resources/js/user/invites.js
@@ -1,19 +1,14 @@
+import {sendRequest} from '../app.js'
+
   const acceptButtons = document.querySelectorAll('#accept')
   for (const acceptButton of acceptButtons) {
     acceptButton.addEventListener('click',() => {
       const inviteId = acceptButton.parentElement.querySelector('input[name=inviteId]').value
       const userId = acceptButton.parentElement.querySelector('input[name=userId]').value
-      const url = `${SERVER}/api/user/${userId}/invites/${inviteId}`
+      const options = {method:"POST",headers{'Content-Type': 'application/json'}}
       // TODO : Precisamos de verificar a resposta
-      fetch(url, {
-        method: "POST",
-        headers: {
-                     'Content-Type': 'application/json'
-                     },
-
-      })
+      sendRequest(`/api/user/${userId}/invites/${inviteId}`,options)
       acceptButton.parent.parent.style.display=none
-      
     })
   }
 
@@ -22,16 +17,9 @@
     declineButton.addEventListener('click',() => {
       const inviteId = declineButton.parentElement.querySelector('input[name=inviteId]').value
       const userId = declineButton.parentElement.querySelector('input[name=userId]').value
-      const url = `${SERVER}/api/user/${userId}/invites/${inviteId}`
       // TODO : Precisamos de verificar a resposta
-      fetch(url, {
-        method: "DELETE",
-        headers: {
-                     'Content-Type': 'application/json'
-                     },
-
-      })
+      const options = {method:"DELETE",headers{'Content-Type': 'application/json'}}
+      sendRequest(`/api/user/${userId}/invites/${inviteId}`,options)
       declineButton.parent.parent.style.display=none
-      
     })
   }
