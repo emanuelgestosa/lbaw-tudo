bc828e44265e3e68f6d5191a7b94a4a95a8571ce
Emanuel Gestosa <emanuel-367@hotmail.com>
2022-12-20 14:37:29 +0000

    admin sub tabs

--- public/js/admin.js
+++ public/js/admin.js
@@ -18,24 +18,79 @@ Array.from(tabs).forEach(e => {
       let subtab_search = document.createElement('button')
       subtab_bans.classList.add('sub-tab')
       subtab_bans.classList.add('selected')
+      subtab_bans.id = "sub-tab-bans"
       subtab_bans.innerHTML = '<i class="fa-solid fa-ban"></i> Banned Users'
       subtab_create.classList.add('sub-tab')
       subtab_create.innerHTML = '<i class="fa-solid fa-plus"></i> Create User'
+      subtab_create.id = "sub-tab-createuser"
       subtab_search.classList.add('sub-tab')
       subtab_search.innerHTML = '<i class="fa-solid fa-search"></i> Search User'
+      subtab_search.id = "sub-tab-searchuser"
       let sub_tabs = document.getElementById('sub-tabs')
       sub_tabs.innerHTML = ''
       sub_tabs.appendChild(subtab_bans)
       sub_tabs.appendChild(subtab_create)
       sub_tabs.appendChild(subtab_search)
+      openBans()
     } else if (e.currentTarget.id == 'tab-project') {
       let subtab_search = document.createElement('button')
       subtab_search.classList.add('sub-tab')
       subtab_search.classList.add('selected')
       subtab_search.innerHTML = '<i class="fa-solid fa-search"></i> Search Project'
+      subtab_search.id = "sub-tab-searchproject"
       let sub_tabs = document.getElementById('sub-tabs')
       sub_tabs.innerHTML = ''
       sub_tabs.appendChild(subtab_search)
+      openSearchProject()
     }
+    subTabsNav()
   })
 })
+
+/* sub-tabs navigation */
+function subTabsNav() {
+  let sub_tabs = document.getElementsByClassName('sub-tab')
+  
+  Array.from(sub_tabs).forEach(e => {
+    e.addEventListener('click', function(e) {
+      selected = document.getElementsByClassName('sub-tab selected') 
+      Array.from(selected).forEach(s => {
+        s.className = s.className.replace(' selected', '')
+      })
+      e.currentTarget.className += ' selected'
+      if (e.currentTarget.id == 'sub-tab-bans') {
+        openBans()
+      } else if (e.currentTarget.id == 'sub-tab-createuser') {
+        openCreateUser()
+      } else if (e.currentTarget.id == 'sub-tab-searchuser') {
+        openSearchUser()
+      } else if (e.currentTarget.id == 'sub-tab-searchproject') {
+        openSearchProject()
+      }
+    })
+  })
+}
+
+function openBans() {
+  let content = document.getElementById('tab-content')
+  content.innerHTML = 'Bans'
+  /* TODO: Bans content */
+}
+
+function openCreateUser() {
+  let content = document.getElementById('tab-content')
+  content.innerHTML = 'Create User'
+  /* TODO: Create User content */
+}
+
+function openSearchUser() {
+  let content = document.getElementById('tab-content')
+  content.innerHTML = 'Search User'
+  /* TODO: Search User content */
+}
+
+function openSearchProject() {
+  let content = document.getElementById('tab-content')
+  content.innerHTML = 'Search Project'
+  /* TODO: Search Project content */
+}
--- resources/views/pages/administration.blade.php
+++ resources/views/pages/administration.blade.php
@@ -15,9 +15,9 @@
       <button id="tab-project" class="tab"><i class="fa-solid fa-project-diagram"></i> Project</button>
     </nav>
     <nav id="sub-tabs">
-      <button class="sub-tab selected"><i class="fa-solid fa-ban"></i> Banned Users</button>
-      <button class="sub-tab"><i class="fa-solid fa-plus"></i> Create User</button>
-      <button class="sub-tab"><i class="fa-solid fa-search"></i> Search User</button>
+      <button id="sub-tab-bans" class="sub-tab selected"><i class="fa-solid fa-ban"></i> Banned Users</button>
+      <button id="sub-tab-createuser" class="sub-tab"><i class="fa-solid fa-plus"></i> Create User</button>
+      <button id="sub-tab-searchuser" class="sub-tab"><i class="fa-solid fa-search"></i> Search User</button>
     </nav>
     <div id="tab-content">
       @foreach (App\Models\User::all() as $user)
