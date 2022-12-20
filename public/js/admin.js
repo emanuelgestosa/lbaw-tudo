0a02471e53d78417ca13864ab52fb7b0ae69bbf0
Emanuel Gestosa <emanuel-367@hotmail.com>
2022-12-20 15:24:33 +0000

    admin create user page

--- public/js/admin.js
+++ public/js/admin.js
@@ -81,8 +81,94 @@ function openBans() {
 
 function openCreateUser() {
   let content = document.getElementById('tab-content')
-  content.innerHTML = 'Create User'
-  /* TODO: Create User content */
+
+  /* create form */
+  let form = document.createElement('form')
+  form.setAttribute('method', 'POST')
+  form.setAttribute('action', '/api/user')
+
+  /* create labels */
+  let labelName = document.createElement('label')
+  labelName.setAttribute('for', 'name')
+  labelName.innerText = 'Name'
+  let labelUsername = document.createElement('label')
+  labelUsername.setAttribute('for', 'username')
+  labelUsername.innerText = 'Username'
+  let labelMail = document.createElement('label')
+  labelMail.setAttribute('for', 'email')
+  labelMail.innerText = 'E-Mail Address'
+  let labelPhone = document.createElement('label')
+  labelPhone.setAttribute('for', 'phone_number')
+  labelPhone.innerText = 'Phone Number'
+  let labelPassword = document.createElement('label')
+  labelPassword.setAttribute('for', 'password')
+  labelPassword.innerText = 'Password'
+  let labelPasswordC = document.createElement('label')
+  labelPasswordC.setAttribute('for', 'password-confirm')
+  labelPasswordC.innerText = 'Confirm Password'
+
+  /* create inputs */
+  let inputName = document.createElement('input')
+  inputName.id = 'name'
+  inputName.setAttribute('type', 'text')
+  inputName.setAttribute('name', 'name')
+  inputName.setAttribute('required', 'required')
+  inputName.setAttribute('autofocus', 'autofocus')
+  let inputUsername = document.createElement('input')
+  inputUsername.id = 'username'
+  inputUsername.setAttribute('type', 'text')
+  inputUsername.setAttribute('name', 'username')
+  inputUsername.setAttribute('required', 'required')
+  let inputMail = document.createElement('input')
+  inputMail.id = 'email'
+  inputMail.setAttribute('type', 'email')
+  inputMail.setAttribute('name', 'email')
+  inputMail.setAttribute('required', 'required')
+  let inputPhone = document.createElement('input')
+  inputPhone.id = 'phone_number'
+  inputPhone.setAttribute('type', 'text')
+  inputPhone.setAttribute('name', 'phone_number')
+  inputPhone.setAttribute('required', 'required')
+  let inputPassword = document.createElement('input')
+  inputPassword.id = 'password'
+  inputPassword.setAttribute('type', 'password')
+  inputPassword.setAttribute('name', 'password')
+  inputPassword.setAttribute('required', 'required')
+  let inputPasswordC = document.createElement('input')
+  inputPasswordC.id = 'password-confirm'
+  inputPasswordC.setAttribute('type', 'password')
+  inputPasswordC.setAttribute('name', 'password_confirmation')
+  inputPasswordC.setAttribute('required', 'required')
+
+  /* submit button */
+  let submit = document.createElement('button')
+  submit.setAttribute('type', 'submit')
+  submit.innerText = 'Create'
+
+  /* csrf token */
+  let token = document.createElement('input')
+  token.setAttribute('type', 'hidden')
+  token.setAttribute('name', '_token')
+  token.setAttribute('value', document.querySelector('meta[name="csrf-token"]').content)
+
+  /* build form */
+  form.appendChild(token)
+  form.appendChild(labelName)
+  form.appendChild(inputName)
+  form.appendChild(labelUsername)
+  form.appendChild(inputUsername)
+  form.appendChild(labelMail)
+  form.appendChild(inputMail)
+  form.appendChild(labelPhone)
+  form.appendChild(inputPhone)
+  form.appendChild(labelPassword)
+  form.appendChild(inputPassword)
+  form.appendChild(labelPasswordC)
+  form.appendChild(inputPasswordC)
+  form.appendChild(submit)
+
+  content.innerHTML = ''
+  content.appendChild(form)
 }
 
 function openSearchUser() {
