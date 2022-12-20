27d526dd916ba94eabde2555f30889ce96e9ddff
Martim <mitram790@gmail.com>
2022-12-20 18:33:48 +0000

    Server now send status 404 when trying to invite a user that doesnt exist to a prject that doesnt exist

--- app/Http/Controllers/ProjectInvitesController.php
+++ app/Http/Controllers/ProjectInvitesController.php
@@ -1,15 +1,18 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Project;
 use App\Models\Invite;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProjectInvitesController extends Controller
 {
     public static function invites($id)
     {
+        if(!Project::find($id)){
+            return response()->json(["Message"=>"Object Does Not Exist"],404);
+        }
         $result = Project::find($id)->invites()->get();
         return response()->json($result);
     }
@@ -18,6 +21,9 @@ class ProjectInvitesController extends Controller
 
         $invitee = $r->input('id_invitee');
         $inviter = $r->input('id_inviter');
+        if(!User::find($invitee) || !User::find($inviter) || !Project::find($id)){
+            return response()->json(["Message"=>"Object Does Not Exist"],404);
+        }
         $inProject = Project::find($id)->collaborators->where('id',$invitee) != [];
         if($inProject){
          return response()->json(["Message" => "User already in project"],400);
