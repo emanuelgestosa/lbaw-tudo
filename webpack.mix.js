4f3e4443a16517866f507e80c01cee3164a68ead
Martim <mitram790@gmail.com>
2022-12-20 16:14:41 +0000

    Webpack approach 1 js file per page

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,7 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.js(['resources/js/app.js',
-        'resources/js/project/invites.js',
-        'resources/js/user/administration.js'],
-        'public/js');
+mix.js('resources/js/app.js','public/js/app.js')
+    .js('resources/js/user/administration.js','public/js/user/administration.js')
+    .js('resources/js/user/invites.js','public/js/user/invites.js')
+    .js('resources/js/project/invites.js','public/js/project/invites.js');
