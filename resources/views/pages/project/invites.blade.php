0bec588ec46647a95d7658412aa3a32dbca14ae1
Martim <mitram790@gmail.com>
2022-12-21 17:04:55 +0000

    Invites Now Have Delete Button

--- resources/views/pages/project/invites.blade.php
+++ resources/views/pages/project/invites.blade.php
@@ -9,12 +9,14 @@
     <section class="invite-content" project-id="{{$project->id}}" user-id="{{Auth::user()->id}}" style="display:flex;gap:5em;margin:auto;">
         <section class="project-invite-list">
             @foreach ($project->invites()->get() as $invite)
-            <article class="project-invite-card">
+            <article class="project-invite-card" project-id="{{$project->id}}" invite-id="{{$invite->id}}">
+                <p class="project-invite-card-title">{{$project->title}}</p>
                 <p>
                     <a href="/user/{{$invite->inviter->id}}">{{$invite->inviter->name}}</a>
                     enviou convite para
                     <a href="/user/{{$invite->inviter->id}}">{{$invite->invited->name}}</a>
-                    </p>
+                </p>
+                <button closed>Delete Invite</button>
             </article>
             @endforeach
         </section>
@@ -29,16 +31,27 @@
 
 </section>
 <style>
-section.project-invite-list{
-    display:flex;
-    flex-direction:column;
-    gap:0.5em;
-}
-article.project-invite-card{
-    border: 1px solid blue;
-    border-radius:1em;
-    padding:1em;
-    
-}
+    section.project-invite-list {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5em;
+    }
+
+    article.project-invite-card {
+        border: 1px solid blue;
+        border-radius: 1em;
+        padding: 1em;
+
+    }
+    article.project-invite-card > button[closed]{
+    display:none;
+    }
+    p.project-invite-card-title {
+        text-align: center;
+        margin: 0;
+        border-bottom: 1px solid blue;
+    }
 </style>
+<script>
+</script>
 @endsection
