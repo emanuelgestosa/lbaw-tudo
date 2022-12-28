b60ede8ca4f845aa1de4586362adcd5f61ccfbd0
Mariana Rocha <root@HejSokoly.localdomain>
2022-12-28 00:24:55 +0000

    vertical moves

--- public/js/project/tasks-sortable.js
+++ public/js/project/tasks-sortable.js
@@ -1,9 +1,18 @@
+const cols = document.getElementsByClassName('board');
 const tasks = document.getElementsByClassName('tareas');
+
 console.log('tou aqui');
+
+for (const col of cols){
+    new Sortable(col, {
+        handle: '.vertical',
+        group : 'board',
+    });
+}
 
 for (const task of tasks){
     new Sortable(task, {
-        handle: '.fa-grip-lines',
+        handle: '.task-container',
         group : 'tareas',
     });
 }
--- resources/views/partials/task/card.blade.php
+++ resources/views/partials/task/card.blade.php
@@ -3,10 +3,11 @@
 <article class="task-card" data-id="{{ $task->id }}">
   <header>
     <a href="{{ url('/task/' . $task->id) }}">
-      <h2 class="task-name">    
-        <i class="fa fa-grip-lines"></i>
-        {{ $task->name}} 
-      </h2>
+      <div class="task-container">
+        <h2 class="task-name">    
+          {{ $task->name}} 
+        </h2>
+      </div>
     </a>
     <!--<h3 class="task-description"> {{$task->description}} </h3>-->
   </header>
