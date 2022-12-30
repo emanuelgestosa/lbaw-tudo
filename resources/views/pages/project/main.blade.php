33e65d9f5cda66f124102e93e299f25aaa526c4a
Mariana Rocha <root@HejSokoly.localdomain>
2022-12-30 04:03:26 +0000

    side nav bar projects

--- resources/views/pages/project/main.blade.php
+++ resources/views/pages/project/main.blade.php
@@ -6,29 +6,37 @@
 
 
 <article class="project" id="project_content">
-    <div class="main-container d-flex">
-        <div class="sidebar" id="side_nav">
-            <div class="header-box px-3 pt-3 pb-4">
-                <h1 class="page_name text-dark"> {{ $project->title }} </h1>
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 shadow-sm shadow-lg" id="sidebarpro">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-decoration-none">
+                        <span class="fs-5 d-none d-sm-inline" id ="title_nav_lat">{{ $project->title }}</span>
+                    </a>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <i class="fa fa-house"></i> <span class="ms-1 d-none d-sm-inline"> Boards</span>
+                            </a>
+                        </li>  
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <i class="fa-solid fa-users"></i> <span class="ms-1 d-none d-sm-inline"> Team</span>
+                            </a>
+                        </li>  
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <i class="fa-solid fa-user-plus"></i> <span class="ms-1 d-none d-sm-inline"> Invite </span>
+                            </a>
+                        </li>       
+                    </ul>
+                </div>
             </div>
-
-            <ul class="list-unstyled px-2">
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"> <i class="fa fa-table"></i> Boards </a></li>
-                <li class=""><a href="#" class="text-decoration-none d-flex justify-content-between">
-                    <span> <i class="fa fa-users"></i> Team </span>
-                    <span class="bg-dark rounded-pill text-white py-0 px-2"> 23 </span> 
-                </a> 
-                </li>
-            </ul>
-            <hr class="h-color mx-3">
-            <ul class="list-unstyled px-2">
-                <li class=""><a href="#" class="text-decoration-none px-3 py-2 d-block"> <i class="fa fa-envelope"></i> Invite </a></li>
-            </ul>
-
-        </div>
-
-        <div class="content">
-
+            <div class="col py-3">
+                <h3>Boards aqui?</h3>
+               
+                    
+            </div>
         </div>
     </div>
 </article>
