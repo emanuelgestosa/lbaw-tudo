eda3b2ca4f4485667cc2859ee3476d738918271a
Martim <mitram790@gmail.com>
2023-01-01 14:55:50 +0000

    New Task Comment Event

---
+++ app/Events/NewTaskComment.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewTaskComment
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+    public $message;
+    public $taskId;
+    public function __construct($message,$taskId)
+    {
+      $this->message = $message;
+      $this->taskId = $taskId;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('task-comments');
+    }
+
+    public function broadcastAs()
+    {
+        return 'event-new-comment-' . $this->taskId;
+    }
+}
