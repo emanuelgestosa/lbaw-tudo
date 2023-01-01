726ce8d4e412681e3581c167c413611b3dae88ea
Emanuel Gestosa <emanuel-367@hotmail.com>
2023-01-01 17:44:11 +0000

    homepage redirects to profile if loggged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -9,6 +11,20 @@ class HomeController extends Controller
      */
     public function show()
     {
-      return view('pages.static.home');
+      if (!Auth::check()) {
+        return view('pages.static.home');
+      } else {
+        $user = Auth::user();
+
+        return view('pages.user.profile', [
+          'id' => $user->id,
+          'username' => $user->username,
+          'name' => $user->name,
+          'email' => $user->email,
+          'phone_number' => $user->phone_number,
+        ]);
+      }
     }
 }
+
+
